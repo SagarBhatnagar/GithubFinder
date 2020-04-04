707c9a389815329dcbbd9018785288d5f1220eb2
SagarBhatnagar <sagarbhatnagar37@gmail.com>
2020-04-04 22:04:54 +0530

    profile page added

--- ui.js
+++ ui.js
@@ -4,11 +4,11 @@ class UI{
     }
     showProfile(user){
         this.profile.innerHTML = `
-            <div class = "card card-body mb-3>
+            <div class = "card card-body mb-3">
                 <div class = "row">
                     <div class = "col-md-3">
                         <img class = "img-fluid mb-2" src = "${user.avatar_url}">                                       
-                        <a href = "${user.html_url}" target = "_blank" class = "btn btn-block btn-primary mb-3">View Profile</a>                
+                        <a href="${user.html_url}" target = "_blank" class = "btn btn-block btn-primary mb-3">View Profile</a>                
                     </div>
                     <div class = "col-md-9">
                         <span class = "badge badge-primary">Public Repos : ${user.public_repos}</span>
@@ -25,6 +25,8 @@ class UI{
                     </div>
                 </div>
             </div>
-        `
+            <h3 class="page-heading mb-4">Latest Repositories</h3>
+            <div id="repos"></div>
+        `;
     }
 }
